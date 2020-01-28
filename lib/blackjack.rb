83e51c933d9f330f1af448bf62557c9215fbccb4
Diana Ponce <dianap2341@gmail.com>
2020-01-28 20:30:49 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,62 @@
 def welcome
-  # code #welcome here
+puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1...11) 
+
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  display_card_total(deal_card+deal_card)
+  deal_card + deal_card
 end
-
-def hit?
-  # code hit? here
-end
-
 def invalid_command
-  # code invalid_command here
+puts  "Please enter a valid command"
+    end
+    
+#def hit?(number)
+#  prompt_user
+ #user_input= get_user_input
+#if user_input=="h"
+#  number += deal_card
+#  elsif user_input =="s"
+#  number
+#else
+#  return invalid_command 
+
+ # end
+#end
+
+def hit?(current_card_value)
+  prompt_user
+  user_input = get_user_input
+  while user_input != "h" && user_input != "s"
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+  if user_input == "h"
+    current_card_value += deal_card
+  end
+  return current_card_value
 end
 
 #####################################################
@@ -39,6 +64,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  round=initial_round
+   until round > 21 do
+  round=hit?(round)
+  display_card_total(round)
+
 end
+end_game(round)
+end
+
     
